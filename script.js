e6e700d229c78ff2d09a5987c25d345391bac2f7
zinoesxvernice <102968619+zinoesxvernice@users.noreply.github.com>
2026-02-06 18:49:11 +0800

    Update script.js

--- script.js
+++ script.js
@@ -107,4 +107,16 @@ playBtn.addEventListener("click", ()=>{
 
 // Generate floating hearts
 const heartsContainer = document.querySelector(".hearts-container");
-function
+function createHeart() {
+  const heart = document.createElement("div");
+  heart.classList.add("heart");
+  heart.style.left = Math.random() * 100 + "%";
+  heart.style.fontSize = 12 + Math.random() * 16 + "px";
+  heartsContainer.appendChild(heart);
+  heart.textContent = "❤️";
+
+  setTimeout(() => {
+    heart.remove();
+  }, 8000); // remove after animation
+}
+setInterval(createHeart, 500);
